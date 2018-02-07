6ade3c2e68c87674b71ce9c464aa1b1c0ae0c86c
adrian <adrian.sebuliba1@gmail.com>
2018-02-07 22:32:52 +0300

    [Feature #15493983] Add logo component

---
+++ src/components/logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Logo = () => (
+    <div>
+        <h3 className="logo text-center mb-3">Doof Recipes App</h3>
+        <p className="grey-text text-center mb-5">Record your favourite recipes </p>
+    </div>
+);
+
+export default Logo;
