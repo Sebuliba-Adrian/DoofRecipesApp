c4cdd2a47bc254cf8afc0e77972d22ed4cd8823c
adrian <adrian.sebuliba1@gmail.com>
2018-02-08 10:14:28 +0300

    [Feature #154939838]  Import and add login route to the router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Registration from './components/authentication/registration';
+import LoginPage from './components/authentication/login';
 
 const App = () => (
   <BrowserRouter>
+    <div>
+    <Route path="/login" component={LoginPage} />
     <Route path="/registration" component={Registration} />
+    </div>
   </BrowserRouter>
 );
 
