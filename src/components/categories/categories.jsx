cace9b9b092e557dbb8582f981718080c47baa0e
adrian <adrian.sebuliba1@gmail.com>
2018-03-05 12:53:24 +0300

    [Feature #154939969] Migrate categories logic to dashboard

--- src/components/categories/categories.jsx
+++ src/components/categories/categories.jsx
@@ -1,224 +1,58 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import NavBar from '../navigation';
-import { APIUrl } from '../../App';
-import Message from '../message';
-import CategoryModal from './category-modal';
-import Category from './category';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import NavBar from "../navigation";
+import { APIUrl } from "../../App";
+import Message from "../message";
+import CategoryModal from "./category-modal";
+import Category from "./category";
 
-export default class Categories extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            token: localStorage.getItem('token'),
-            categories: null,
-            message: null,
-        };
-        // if (this.state.token !== null) {
-
-        //     console.log(this.state.token);
-        //     this.categoryTransaction('getCategories', 'categories', 'GET');
-        // }
-    }
-
-    logoutUser = () => {
-        this.categoryTransaction('logoutUser', 'logout', 'GET');
-    }
-
-    componentDidMount(){
-
-        axios.interceptors.request.use(config => {
-          const token = localStorage.getItem("token");
-          config.headers.Authorization = `Bearer ${this.state.token}`;
-          return config;
-        });
-        axios
-          .get(`${APIUrl}categories`)
-          .then(res => {
-            const returns = res.data;
-            console.log(returns);
-
-            this.setState({ message: null, categories: returns });
-
-            //     });
-          })
-          .catch(function(error) {
-            this.setState({
-              message:
-                "You don't have any categories. Use the above button to add some.",
-              categories: null
-            });
-          });
-    }
-    categoryTransaction = (action, urlEndPoint, requestMethod, requestBody) => {
-        this.setState({
-            message: 'Processing...',
-        });
-
-        switch (requestMethod) {
-
-    
-            case "POST": {
-                console.log("This is a post request in action")
-                const { name, description } = requestBody;
-
-                axios.post(`${APIUrl}${urlEndPoint}`, { name, description })
-                    .then((response) => {
-                        console.log("xxxxxxxx" + response.data.message)
-                        const stateCopy = Object.assign({}, this.state.categories);
-                        stateCopy.categories = [...Object.values(this.state.categories)[0], response.data]
+export default function Categories(props) {
 
 
-                        this.setState({
-                            categories: stateCopy,
-                            message: response.data.message
-                        });
-                    })
-                    .catch((error) => {
-                        if (error.response && error.response.data) {
-                            this.setState({
-                                message: error.response.data.message,
-                            });
-                        }
-                    });}
-                break;
-
-            case "PUT": {
-                const { name, description } = requestBody;
-                
-
-                axios.put(`${APIUrl}${urlEndPoint}`, { name, description })
-                    .then(function (response) {
-                        
-                        const stateCopy = Object.assign({}, this.state.categories);
-                        //Find index of specific object using findIndex method.    
-                        const objIndex = stateCopy.categories.findIndex(category => category.id == requestBody.id);
-                       
-                        stateCopy.categories[objIndex].name=requestBody.name;
-                        stateCopy.categories[objIndex].description = requestBody.description;
-                           
-                        
-                        this.setState({
-                            message: 'Category successfully updated.',
-                            categories: stateCopy,
-                        });
-                    }.bind(this))
-                    .catch(function (error) {
-                        console.log(error)
-                    });
-                             }
-                break;
-
-            case "DELETE":
-            
-                axios.delete(`${APIUrl}${urlEndPoint}`)
-                  .then(res => {
-                    
-                    const stateCopy = Object.assign({}, this.state.categories);
-                    //Find index of specific object using findIndex method.
-                    const objIndex = stateCopy.categories.findIndex(category => category.id ==parseInt(urlEndPoint.substring(urlEndPoint.lastIndexOf("/") + 1), 10));
-                    stateCopy.categories.splice(objIndex, 1);
-                    debugger;
-
-                    this.setState({
-                        message:"Category successfully deleted.",
-                        categories: stateCopy
-                    });
-                  });
-
-                break;
-
-
-
-            default:
-
-                localStorage.removeItem('token');
-                localStorage.removeItem('username');
-                this.setState({
-                    message: 'You have successfully logged out.',
-                });
-                break;
-
-
-
-
-
-
-
-        }
-
-    }
-
-    categoryRow = (category, index) => {
-
-        return (
-            <Category
-                key={category.id}
-                category={category}
-                setActiveCategory={this.setActiveCategory}
-                categoryTransaction={this.categoryTransaction}
-            />);
-
-    }
-
-    search(){
-     
-        alert("this.refs.keyword.value"); 
-
-    }
-
-
-
-
-    render() {
-        const { categories, message } = this.state;
-        if (!this.state.token) {
-            return <Redirect to={{ pathname: '/login', data: { message: message } }} />;
-        }
-        return (
-            <div>
-                <div className="custom-navbar">
-                    <NavBar search={this.search} logoutUser={this.logoutUser} />
-                </div>
-                <div className="container">
-                    <div className="card mt-3 mb-3">
-                        <div className="card-block">
-                            <div className="d-flex w-100 justify-content-between mb-2">
-                                <p />
-                                <button
-                                    className="btn btn-sm btn-primary col-xs-12"
-                                    data-toggle="modal"
-                                    data-target="#addCategoryModal"
-                                ><span className="fa fa-plus pull-left" /> Add category</button>
-                            </div>
-                            {
-                                this.state != null && message &&
-                                <Message
-                                    message={message}
-                                />
-                            }
-                            {
-                                this.state && categories && categories.length !== 0 &&
-                                <div>
-                                    <h6 className="card-text ml-4 pb-2">
-                                        Categories <small className="text-muted">select to view details</small>
-                                    </h6>
-                                    <ul className="list-group list-group-flush">
-                                        {categories.categories.map(this.categoryRow)}
-                                    </ul>
-                                </div>
-                            }
-                        </div>
-                    </div>
-                </div>
-                <CategoryModal
-                    title="Create a category"
-                    action="Submit"
-                    theId="addCategoryModal"
-                    categoryTransaction={this.categoryTransaction}
-                />
+  return (
+    <div>
+      <div className="container">
+        <div className="card mt-3 mb-3">
+          <div className="card-block">
+            <div className="d-flex w-100 justify-content-between mb-2">
+              <p />
+              <button
+                className="btn btn-sm btn-primary col-xs-12"
+                data-toggle="modal"
+                data-target="#addCategoryModal"
+              >
+                <span className="fa fa-plus pull-left" /> Add category
+              </button>
             </div>
-        );
-    }
+            {props.categories &&
+              props.categories.length !== 0 && (
+                <div>
+                  <h6 className="card-text ml-4 pb-2">
+                    Categories{" "}
+                    <small className="text-muted">select to view details</small>
+                  </h6>
+                  <ul className="list-group list-group-flush">
+                    {props.categories.map(category => (
+                        
+                      <Category
+                        key={category.id}
+                        category={category}
+                        request={props.request}
+                        viewRecipes={props.viewRecipes}
+                      />
+                    )) }
+                  </ul>
+                </div>
+              )}
+          </div>
+        </div>
+      </div>
+      <CategoryModal
+        title="Create a category"
+        action="Submit"
+        theId="addCategoryModal"
+        request={props.request}
+      />
+    </div>
+  );
 }
--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,35 +1,320 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import NavBar from './navigation';
-import Message from './message';
-import { APIUrl } from '../App';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import NavBar from "./navigation";
+import Message from "./message";
+import Categories from "./categories/categories";
+import Recipes from "./recipes/recipes";
+import { APIUrl } from "../App";
+import axios from "axios";
 
 export default class Dashboard extends Component {
-    constructor(props) {
-        super(props);
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: localStorage.getItem("token"),
+      showing: "categories",
+      message: null,
+      categories: [],
+      selectedCategory: null,
+      recipes: []
+    };
+    if (this.state.token) {
+      this.request("getCategories", "categories", "GET");
     }
+  }
 
+  componentDidMount() {
+    axios.interceptors.request.use(config => {
+      const token = localStorage.getItem("token");
+      config.headers.Authorization = `Bearer ${this.state.token}`;
+      return config;
+    });
+    axios
+      .get(`${APIUrl}categories`)
+      .then(res => {
+        const { categories } = res.data;
 
-    render() {
-        
-        return (
-            <div>
-                <div className="custom-navbar">
-                    <NavBar />
-                </div>
-                <div className="row">
-                    <div className="col-4 ">
-                      LEFT
-                    </div>
+        this.setState({ categories: categories });
 
-                    <div className="col-8 items">
-                        RIGHT
-                    </div>
-                </div>
-                
+        //     });
+      })
+      .catch(error => {
+        this.setState({
+          message:
+            "You don't have any categories. Use the above button to add some.",
+          categories: null
+        });
+      });
+  }
 
-            </div>
+  viewRecipes = selectedCategory => {
+    this.setState({
+      showing: "recipes",
+      selectedCategory
+    });
+    this.request(
+      "getRecipes",
+      `categories/${selectedCategory.id}/recipes`,
+      "GET"
+    );
+  };
+
+  viewCategories = () => {
+    this.request("getCategories", "categories", "GET");
+    this.setState({
+      showing: "categories",
+      recipes: null,
+      selectedCategory: null
+    });
+  };
+
+  search = searchTerm => {
+    if (searchTerm.length > 0) {
+      this.request(
+        "searchCategories",
+        `categories?q=${searchTerm.substring(2)}`,
+        "GET"
+      );
+    }
+  };
+
+  request = (action, urlEndPoint, requestMethod, requestBody) => {
+    this.setState({ message: "Processing..." });
+    if (action === "getCategory") {
+      return axios
+        .get(`${APIUrl}categories`)
+        .then(response => {
+          const { categories } = response.data;
+          console.log(categories);
+
+          this.setState({ message: null, categories: categories });
+
+          //     });
+        })
+        .catch(function(error) {
+          this.setState({
+            message:
+              "You don't have any categories. Use the above button to add some.",
+            categories: null
+          });
+        });
+    } else if (action === "searchCategories") {
+      return axios
+        .get(`${APIUrl}${urlEndPoint}`)
+        .then(response => {
+          const { categories } = response.data;
+          console.log(categories);
+
+          this.setState({ message: null, categories: categories });
+
+          //     });
+        })
+        .catch(error => {
+          this.setState({
+            message: error.response.data.message,
+            categories: null
+          });
+        });
+    } else if (action === "addCategory") {
+      const { name, description } = requestBody;
+
+      return axios
+        .post(`${APIUrl}${urlEndPoint}`, { name, description })
+        .then(response => {
+          let stateCopy = [
+            ...this.state.categories,
+            Object.assign({}, response.data)
+          ];
+
+          this.setState({
+            categories: stateCopy,
+            message: response.data.message
+          });
+          this.componentDidMount();
+        })
+        .catch(error => {
+          if (error.response && error.response.data) {
+            this.setState({
+              message: error.response.data.message
+            });
+          }
+        });
+    } else if (action === "updateCategory") {
+      const { name, description } = requestBody;
+
+      return axios
+        .put(`${APIUrl}${urlEndPoint}`, { name, description })
+        .then(
+          function(response) {
+            let categoriesCopy = [...this.state.categories];
+            //Find index of specific object using findIndex method.
+            const objIndex = categoriesCopy.findIndex(
+              category => category.id == requestBody.id
+            );
+
+            categoriesCopy[objIndex].name = requestBody.name;
+            categoriesCopy[objIndex].description = requestBody.description;
+
+            this.setState({
+              message: response.data.message,
+              categories: categoriesCopy
+            });
+            console.log(this.state.recipes);
+          }.bind(this)
+        )
+        .catch(function(error) {
+          console.log(error);
+        });
+    } else if (action === "deleteCategory") {
+      return axios.delete(`${APIUrl}${urlEndPoint}`).then(response => {
+        const stateCopy = [...this.state.categories];
+        //Find index of specific object using findIndex method.
+        const objIndex = stateCopy.findIndex(
+          category =>
+            category.id ==
+            parseInt(
+              urlEndPoint.substring(urlEndPoint.lastIndexOf("/") + 1),
+              10
+            )
         );
+
+        stateCopy.splice(objIndex, 1);
+
+        this.setState({
+          message: response.data.message,
+          categories: stateCopy
+        });
+        this.componentDidMount();
+      });
+    } else if (action === "logoutUser") {
+      localStorage.removeItem("token");
+      localStorage.removeItem("username");
+      this.setState({
+        message: "Logged out successfully.",
+        token: null
+      });
+    } else if (action === "getRecipes") {
+      return axios
+        .get(`${APIUrl}${urlEndPoint}`)
+        .then(res => {
+          const { recipes } = res.data;
+
+          this.setState({
+            message: null,
+            recipes: recipes
+          });
+        })
+        .catch(error => {
+          this.setState({
+            message:
+              "You don't have any recipes in this category. Use the add button above to add some.",
+            recipes: []
+          });
+        });
+    } else if (action === "addRecipe") {
+      const { name, description } = requestBody;
+
+      return axios
+        .post(`${APIUrl}${urlEndPoint}`, { name, description })
+        .then(response => {
+          let stateCopy = [
+            ...this.state.recipes,
+            Object.assign({}, response.data)
+          ];
+
+          this.setState({
+            recipes: stateCopy,
+            message: response.data.message
+          });
+        })
+        .catch(error => {
+          if (error.response && error.response.data) {
+            this.setState({ message: error.response.data.message });
+          }
+        });
+    } else if (action === "updateRecipe") {
+      const { name, description } = requestBody;
+
+      return axios
+        .put(`${APIUrl}${urlEndPoint}`, { name, description })
+        .then(
+          function(response) {
+            let recipesCopy = [...this.state.recipes];
+            //Find index of specific object using findIndex method.
+            const objIndex = recipesCopy.findIndex(
+              recipe => recipe.id == requestBody.id
+            );
+
+            recipesCopy[objIndex].name = requestBody.name;
+            recipesCopy[objIndex].description = requestBody.description;
+
+            this.setState({
+              message: response.data.message,
+              recipes: recipesCopy
+            });
+            console.log(this.state);
+          }.bind(this)
+        )
+        .catch(function(error) {
+          console.log(error);
+        });
+    } else if (action === "deleteRecipe") {
+      return axios.delete(`${APIUrl}${urlEndPoint}`).then(response => {
+        const stateCopy = [...this.state.recipes];
+        //Find index of specific object using findIndex method.
+        const objIndex = stateCopy.findIndex(
+          recipe =>
+            recipe.id ==
+            parseInt(
+              urlEndPoint.substring(urlEndPoint.lastIndexOf("/") + 1),
+              10
+            )
+        );
+
+        stateCopy.splice(objIndex, 1);
+
+        this.setState({
+          message: response.data.message,
+          recipes: stateCopy
+        });
+      });
     }
+  };
+
+  render() {
+    if (!this.state.token) {
+      return (
+        <Redirect
+          to={{ pathname: "/", data: { message: this.state.message } }}
+        />
+      );
+    }
+    return (
+      <div>
+        <div className="custom-navbar bg-cool-blue">
+          <NavBar
+            request={this.request}
+            viewCategories={this.viewCategories}
+            search={this.search}
+          />
+        </div>
+        {this.state != null &&
+          this.state.message && <Message message={this.state.message} />}
+        {this.state.showing === "categories" ? (
+          <Categories
+            categories={this.state.categories}
+            request={this.request}
+            viewRecipes={this.viewRecipes}
+          />
+        ) : (
+          <Recipes
+            selectedCategory={this.state.selectedCategory}
+            recipes={this.state.recipes}
+            request={this.request}
+            viewCategories={this.viewCategories}
+          />
+        )}
+      </div>
+    );
+  }
 }
