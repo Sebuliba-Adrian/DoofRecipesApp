5b4c86759902c3ef66d4062822c6365901e32767
adrian <adrian.sebuliba1@gmail.com>
2018-03-05 13:09:05 +0300

    [Feature #154939969] Add logout feature on the navigation bar

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,37 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
 
-let NavBar = (prop) => {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-4">
-                    <Link to="/dashboard" className="navbar-brand logo">
-                        <h3>Doof!</h3>
-                    </Link>
-                </div>
-                <div className="col-md-4">
-                    <input
-                        className="form-control mr-sm-2 search-box"
-                        type="text"
-                        placeholder="Search"
-                        
-                    />
-                    <input
-                        className="btn btn-primary"
-                        type="button"
-                        placeholder="Search"
-                        onClick={prop.search}
-                    />
-                </div>
-                <div className="col-md-4 text-right logo ">
-                    <a className="nav-link" href="#">
-                        {localStorage.getItem('username')} <span className="fa fa-sign-out" />
-                    </a>
-                </div>
-            </div>
+export default function NavBar(props) {
+  function logoutUser(event) {
+    event.preventDefault();
+    props.request("logoutUser", "logout", "GET");
+  }
+  function viewCategories(event) {
+    event.preventDefault();
+    props.viewCategories();
+  }
+  function triggerSearch(event) {
+    
+      props.search(event.target.value);
+
+  }
+  return (
+    <div className="">
+      <div className="row">
+        <div className="col-md-4">
+          <a
+            href=""
+            onClick={viewCategories}
+            className="navbar-brand logo text-white"
+          >
+            <h3>
+              <span className="fa fa-lg fa-cutlery mr-3 ml-3" />| &nbsp; Doof!
+              recipes
+            </h3>
+          </a>
         </div>
-    );
+        <div className="col-md-4 text-center">
+          <input
+            className="form-control mr-sm-2 search-box"
+            type="text"
+            placeholder="Search e.g c:the category name"
+            onKeyDown={triggerSearch}
+          />
+        </div>
+        <div className="col-md-4 text-right">
+          <div className="dropdown logout-dropdown">
+            <div
+              className="fa fa-user-circle fa-lg text-white dropdown-toggle"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+            >
+              &nbsp;<span className="logo">
+                <b> {localStorage.getItem("username")} </b>
+              </span>
+            </div>
+            <div
+              className="dropdown-menu dropdown-cust"
+              aria-labelledby="dropdownMenuLink"
+            >
+              <a
+                id="logoutLink"
+                className="dropdown-item"
+                href=""
+                onClick={logoutUser}
+              >
+                Logout
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default NavBar;
