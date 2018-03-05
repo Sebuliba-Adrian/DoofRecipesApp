5d88f341654f320b0d5ab122d754431e1d81d513
adrian <adrian.sebuliba1@gmail.com>
2018-03-05 13:02:59 +0300

    [Feature #154939969] Add formatting

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 
 export default function Message({ message }) {
-    return (
-        <div className={'message alert alert-dismissible fade show alert-info'} role="alert">
-         <button
-                type="button"
-                className="close fa fa-close align-middle"
-                data-dismiss="alert"/>
-            {message}
-        </div>
-    );
+    return <div className={"message alert alert-dismissible fade show alert-info no-border-corners"} role="alert">
+        <button type="button" className="close fa fa-close align-middle" data-dismiss="alert" />
+        {message}
+      </div>;
 }
