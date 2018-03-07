4def9f40b87ebbe931839836b824602c81003345
adrian <adrian.sebuliba1@gmail.com>
2018-03-07 22:31:39 +0300

    [Feature #154939969] Add PageNot found component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Registration from "./components/authentication/registration";
 import LoginPage from "./components/authentication/login";
 import Dashboard from "./components/dashboard";
+import LostPage from "./components/LostPage"
 
 const App = () => (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/registration" component={Registration} />
-      <Route path="(/|/login)" component={LoginPage} />
-      <Route path="(/dashboard)" component={Dashboard} />
-    </div>
+      <Route path="(/|/login)" exact component={LoginPage} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="*" exact={true} component={LostPage} />
+    </Switch>
   </BrowserRouter>
 );
 
