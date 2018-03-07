149ed15748ebb45de9f57a4c5b41f8ed6cd9c1a4
adrian <adrian.sebuliba1@gmail.com>
2018-03-07 22:36:03 +0300

    [Feature #154939969]  Refactor Category component

--- src/components/categories/category.jsx
+++ src/components/categories/category.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import DateDisplay from "../display-date";
 import CategoryModal from "./category-modal";
 import ConfirmDelete from "../confirm-delete";
 
@@ -15,53 +14,48 @@ export default function Category(props) {
   }
 
   return (
-      <div>
-    <a
-      href=""
-      onClick={viewRecipes}
-      className="list-group-item list-group-item-action flex-column align-items-start"
-    >
-      <div className="container">
-        <div className="row">
-          <div className="col-md-2 hidden-sm-down">
-            <DateDisplay date={props.category.date_created} />
+    <div>
+      <a
+        href=""
+        onClick={viewRecipes}
+        className="list-group-item list-group-item-action flex-column align-items-start"
+      >
+        <div className="d-flex w-100 justify-content-between">
+          <div>
+            {props.category.date_modified && (
+              <small className="text-muted">
+                <strong>Updated: </strong>
+                {props.category.date_modified.substring(4)}
+              </small>
+            )}
           </div>
-          <div className="col-md-10 col-sm-12">
-            <div className="d-flex w-100 justify-content-between">
-              <div>
-                {props.category.date_modified && (
-                  <small className="text-muted">
-                    <strong>Updated: </strong>
-                    {props.category.date_modified.substring(4)}
-                  </small>
-                )}
-              </div>
-              <div>
-                <div
-                  className="fa fa-lg fa-pencil-square-o mr-2"
-                  data-toggle="modal"
-                  data-target={`#${props.category.id}`}
-                  data-backdrop="static"
-                  data-keyboard="false"
-                  onClick={handleClick}
-                />
-                <div
-                  className="fa fa-lg fa-trash-o ml-2"
-                  data-toggle="modal"
-                  data-target={`#deleteCategoryModel${props.category.id}`}
-                  data-backdrop="static"
-                  data-keyboard="false"
-                  onClick={handleClick}
-                />
-              </div>
-            </div>
-            <br />
-            <h4 className="mb-1">{props.category.name}</h4>
-            <h6>{props.category.description}</h6>
+          <div>
+            <div
+              className="fa fa-lg fa-pencil-square-o mr-2"
+              data-toggle="modal"
+              data-target={`#${props.category.id}`}
+              data-backdrop="static"
+              data-keyboard="false"
+              onClick={handleClick}
+            />
+            <div
+              className="fa fa-lg fa-trash-o ml-2"
+              data-toggle="modal"
+              data-target={`#deleteCategoryModel${props.category.id}`}
+              data-backdrop="static"
+              data-keyboard="false"
+              onClick={handleClick}
+            />
           </div>
         </div>
-      </div>
-       </a>
+        <br />
+        <h4 className="mb-1">{props.category.name}</h4>
+        <h6>{props.category.description}</h6>
+        <small className="text-muted">
+          <strong>Created: </strong>
+          {props.category.date_created.substring(4)}
+        </small>
+      </a>
       <CategoryModal
         title="Edit a category"
         action="Submit"
@@ -69,13 +63,12 @@ export default function Category(props) {
         category={props.category}
         request={props.request}
       />
-     
+
       <ConfirmDelete
         theId={`deleteCategoryModel${props.category.id}`}
         category={props.category}
         request={props.request}
       />
-      </div>
-    
+    </div>
   );
 }
