a7882eee1dc18316da28aedd9150d84cfea9106f
adrian <adrian.sebuliba1@gmail.com>
2018-03-07 22:25:55 +0300

    [Feature #1549399969] Modify styles for date display

--- src/components/display-date.jsx
+++ src/components/display-date.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import '../static/style/date-display.css';
+import React from "react";
+import "../static/style/date-display.css";
 
-export default function DateDisplay({date}) {
-  
-    return (
-        <div className="card text-center date-display">
-            <div className="card-header text-white bg-primary">
-                {date.substring(7, 11)}
-            </div>
-            <div className="card-text"><h4>{date.substring(4, 7)}</h4></div>
-            <div className="card-footer">{date.substring(11, 16)}</div>
-        </div>
-    );
+export default function DateDisplay({ date }) {
+  return (
+    <div className="text-center date-display">
+      <div className="date-display-month text-white bg-cool-blue">
+        {date.substring(7, 11)}
+      </div>
+      <div className="date-display-day">
+        <h4>{date.substring(4, 7)}</h4>
+      </div>
+      <div className="date-display-year">{date.substring(11, 16)}</div>
+    </div>
+  );
 }
