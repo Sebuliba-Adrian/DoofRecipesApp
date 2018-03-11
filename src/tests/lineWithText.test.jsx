3c59600bcd1a0f8a78fd656a0f7105868a0b1c71
adrian <adrian.sebuliba1@gmail.com>
2018-03-11 19:07:09 +0300

    [Chore #154939004] Add snapshot test for lineWithText component

---
+++ src/tests/lineWithText.test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import LineWithText from "../components/authentication/line-with-text";
+
+const props = {
+  lineText: "login"
+};
+
+it("renders correctly", () => {
+  const lineWithText = renderer.create(<LineWithText {...props} />).toJSON();
+  expect(lineWithText).toMatchSnapshot();
+});
