be72578e538aa37c6f6be18feb72d1df4f71cedf
adrian <adrian.sebuliba1@gmail.com>
2018-03-11 19:21:47 +0300

    [Chore #15493900] Add snapshot tests for the footer component

---
+++ src/tests/footer.test.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import renderer from "react-test-renderer";
+import Footer from "../components/authentication/footer";
+
+const props = {
+  message: "success",
+  link: "/login",
+  linkText: "login"
+};
+
+it("renders correctly", () => {
+  const footer = renderer
+    .create(
+      <BrowserRouter>
+        <Footer {...props} />
+      </BrowserRouter>
+    )
+    .toJSON();
+  expect(footer).toMatchSnapshot();
+});
