8cfefe0433d620bc7c03a2a5cf8767eec9dffb0e
adrian <adrian.sebuliba1@gmail.com>
2018-03-12 03:27:36 +0300

    [Chore #154939004] Add tests for confirm delete dialog box component

--- src/tests/registration.test.jsx
+++ src/tests/registration.test.jsx
@@ -26,45 +26,5 @@ describe("Test registration", () => {
       .toJSON();
     expect(registration).toMatchSnapshot();
   });
-  configure({ adapter: new Adapter() });
-  const register = shallow(<Registration {...props} />);
-
-
-  const instance = register.instance();
-  global.fetch = jest.fn(() => {
-       
-            return Promise.resolve({});
-        
-    });
-
-  it("Sets user credentials in state on change", () => {
-    const usernameInput = register.find("[type='text']").at(0);
-    usernameInput.simulate("change", {
-      target: {
-        name: "username",
-        value: "Adrian"
-      }
-    });
-
-    register.setState({
-      registered: false,
-      message: "Test message"
-    });
-    expect(register.state().username).toEqual("Adrian");
-  });
-
-  it("should submit form data", () => {
-    let form = shallow(<Registration />);
-    let handleSubmit = jest.fn(); // <= this doesn't work!!
-
-    form.find("[type='submit']").simulate("click");
-
-    // expect(form.find(".submit-btn").length).toEqual(1);
-    expect(handleSubmit.mock.calls.length).toBe(0);
-  });
-//    it("Registers a user", () => {
-//      const button = register.find("[type='submit']");
-//      button.simulate("submit");
-//     expect(global.fetch).toHaveBeenCalled();
-//    });
+  
 });
